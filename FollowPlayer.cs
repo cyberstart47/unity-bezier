3b50b11664c2fafe1e9a47397676cdde13768b4e
Andrew Lockett <98281211+cyberstart47@users.noreply.github.com>
2022-09-21 12:19:22 -0500

    Add files via upload

---
+++ FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(0, 0, GameObject.Find("Vehicle").GetComponent<Vehicle>().speed);
+        }
+        
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vehicle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed ;
+    public float turnspeed;
+
+    public Vehicle(float speed,float turnspeed)
+    {
+        this.speed = speed;
+        this.turnspeed = turnspeed;
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move Vehicle Forward
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(0, 0, speed);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(Vector3.left * turnspeed * Time.deltaTime);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(Vector3.right * turnspeed * Time.deltaTime);
+        }
+
+    }
+}
